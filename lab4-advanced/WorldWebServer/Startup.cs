fb20a3680c9678142dbce81edad95e9db3bcd10f
Denis <igehuc.dev@gmail.com>
2018-01-25 00:20:33 +0300

    Added Swashbuckle package to generate Swagger description and UI

--- lab4-advanced/WorldWebServer/Startup.cs
+++ lab4-advanced/WorldWebServer/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace WorldWebServer
 {
@@ -24,6 +25,16 @@ namespace WorldWebServer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("world", new Info
+                    {
+                        Title = "World API",
+                        Version = "v1",
+                        Description = "Super-duper world countries and cities storage"
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +47,12 @@ namespace WorldWebServer
 
             app.UseMvc();
             app.UseStaticFiles();
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/world/swagger.json", "World API V1");
+            });
         }
     }
 }
